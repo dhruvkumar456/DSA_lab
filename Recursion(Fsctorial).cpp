b2fc0e3458718aac7b6e7579327aa8b1e731202f
dhruvkumar456 <43489781+dhruvkumar456@users.noreply.github.com>
2019-09-25 22:01:36 +0530

    Create Recursion(Fsctorial).cpp

---
+++ Recursion(Fsctorial).cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n==1)
+    return 1;
+    else
+    return n*factorial(n-1);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int ans=factorial(n);
+    cout<<ans<<endl;
+}
