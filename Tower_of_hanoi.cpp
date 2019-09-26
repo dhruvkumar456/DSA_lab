d74db2a86e6e0797cf890d528f03709ee9080301
dhruvkumar456 <43489781+dhruvkumar456@users.noreply.github.com>
2019-09-26 07:49:46 +0530

    Create Tower_of_hanoi.cpp

---
+++ Tower_of_hanoi.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void toh(int n,char beg,char aux,char end)
+{
+	if(n==1)
+	cout<<beg<<"->"<<end<<endl;
+	else
+	{
+		toh(n-1,beg,end,aux);
+		cout<<beg<<"->"<<end<<endl;
+		toh(n-1,aux,beg,end);
+	}
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	toh(n,'A','B','C');
+}
