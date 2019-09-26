2bbe51f9308a8a0263bc9d8fba23d6bf873bce92
dhruvkumar456 <43489781+dhruvkumar456@users.noreply.github.com>
2019-09-26 09:43:43 +0530

    Create Array(Bubble_sort,Binary_search).cpp

---
+++ Array(Bubble_sort,Binary_search).cpp
@@ -0,0 +1,58 @@
+//ROLL-NO->IIIT18153
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+}  
+  
+void bubbleSort(int arr[], int n)  
+{  
+    int i, j;  
+    for (i = 0; i < n-1; i++)      
+    for (j = 0; j < n-i-1; j++)  
+        if (arr[j] > arr[j+1])  
+            swap(&arr[j], &arr[j+1]);  
+}  
+
+void printArray(int arr[], int size)  
+{  
+    int i;  
+    for (i = 0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}  
+  
+int binarySearch(int *arr, int l, int r, int x) 
+{ 
+    if (r >= l) { 
+        int mid = l + (r - l) / 2; 
+
+        if (arr[mid] == x) 
+            return mid; 
+        if (arr[mid] > x) 
+            return binarySearch(arr, l, mid - 1, x); 
+  
+        return binarySearch(arr, mid + 1, r, x); 
+    } 
+    return -1; 
+} 
+ 
+int main()  
+{  
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+     cin>>arr[i];
+    bubbleSort(arr, n);  
+    printArray(arr, n);
+    int find;
+    cin>>find;
+    int loc=binarySearch(arr, 0, n-1, find);
+    cout<<loc;
+    return 0;  
+}  
